aab1c40331a01dd8ef0e7240be72efeb8b0c6d18
AlexandraDimache <dimachealexandra00@gmail.com>
2023-08-24 09:36:35 +0300

    Second Commit

--- Course8/Selectors/Test7_CSS.py
+++ Course8/Selectors/Test7_CSS.py
@@ -1,2 +1,43 @@
 # ● CSS
 
+''''
+● CSS
+# = identificator pentru ID
+. = identificator pentru clasa
+[] = identificator pentru atribut = valoare
+* = marcator pentru placeholder valoare partiala
+'''
+
+from time import sleep
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+
+# initializam Chrome
+s = Service(ChromeDriverManager().install())
+#stocam serviciul de Chrome, printr-o variabila definita pentru a deschide acel chrome
+chrome = webdriver.Chrome(service=s)
+
+# maximizam fereastra
+chrome.maximize_window()
+
+# navigare catre un url
+chrome.get('https://formy-project.herokuapp.com/form')
+
+
+# selector by Class, ia prima optiune, este ok doar daca avem clasa unica
+chrome.find_element(By.CLASS_NAME, 'form-control').send_keys('Test')
+
+
+# gasim mai multe si le punem in lista
+chrome.find_elements(By.CLASS_NAME, 'form-control')[1].send_keys('Test1')
+
+'''
+div - parinte 
+input - copil care contine textul 'last name'
+sleep(3)
+'''
+
+sleep(3)
+chrome.quit()
--- Course8/Selectors/Test8_Xpath.py
+++ Course8/Selectors/Test8_Xpath.py
@@ -1,1 +1,54 @@
-# XPATH  
+# XPATH
+# XPATH
+
+'''
+Este adresa elementului, prin care putem gasi
+selector by xpath - in f de textul partial - luam textul de pe el cu proprietatea text
+'''
+
+from time import sleep
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+
+# initializam Chrome
+s = Service(ChromeDriverManager().install()) #stocam serviciul de Chrome, printr-o variabila definita pentru a deschide acel chrome
+chrome = webdriver.Chrome(service=s)
+
+# maximizam fereastra
+chrome.maximize_window()
+
+# navigare catre un url
+chrome.get('https://formy-project.herokuapp.com/form')
+
+# selector by XPATH
+chrome.find_element(By.XPATH, '//input[@id="first-name"]').send_keys('A')
+
+# selector by XPATH - toate elementele care respecta regula
+chrome.find_element(By.XPATH, '//*[@id="first-name"]').send_keys('L')
+# * inseamna un inlocuitor pentru toate elementele care respecta regula
+
+
+# selector by XPATH - navigare in jos - trecem prin fiecare element
+chrome.find_element(By.XPATH, '//div/div/input[@id="first-name"]').send_keys('E')
+
+
+# selector by Xpath - navigare in jos - skip tags - cautam oriunde sub forma de input care sa respecte regula
+chrome.find_element(By.XPATH, '//form//input[@id="first-name"]').send_keys('X')
+# // = orice mostenitor, / = primul mostenitor
+
+
+# selector by XPATH - selectare element din lista - index incepe de la 1
+chrome.find_element(By.XPATH, '(//input[@class="form-control"])[2]').send_keys('A')
+
+
+'''
+div - parinte 
+input - copil care contine textul 'last name'
+sleep(3)
+'''
+
+
+sleep(3)
+chrome.quit()
